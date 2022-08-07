2b443875d53269a7c460c5f902864fabdaa9b1f4
Jacob Shuman <44483276+jacob-shuman@users.noreply.github.com>
2022-08-07 17:50:36 -0400

    feat: add iconify

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,8 +1,9 @@
-<script>
+<script lang="ts">
+	import Icon from '@iconify/svelte';
 	import toast from 'svelte-french-toast';
 </script>
 
-<main class="h-screen bg-black flex items-center justify-center text-white flex flex-col">
+<main class="h-screen bg-white flex flex-col justify-center items-center">
 	<h1 class="font-bold text-center text-3xl">This template is powered by</h1>
 
 	<button class="h-12"> Svelte </button>
@@ -10,4 +11,6 @@
 	<button class="h-12"> TailwindCSS </button>
 
 	<button on:click={() => toast.success('This is a toast example!')}> Show Toast </button>
+
+	<Icon icon="akar-icons:folder-add" width="150" height="150" />
 </main>
