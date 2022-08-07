f72c935e1db1014d133e738c2bbe8cacf280ea9e
Jacob Shuman <44483276+jacob-shuman@users.noreply.github.com>
2022-08-07 14:25:29 -0400

    feat: add svelte toast and tailwind

---
+++ src/routes/__layout.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+	import { Toaster } from 'svelte-french-toast';
+	import '../app.css';
+</script>
+
+<main>
+	<slot />
+</main>
+
+<Toaster />
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,13 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+	import toast from 'svelte-french-toast';
+</script>
+
+<main class="h-screen bg-black flex items-center justify-center text-white flex flex-col">
+	<h1 class="font-bold text-center text-3xl">This template is powered by</h1>
+
+	<button class="h-12"> Svelte </button>
+	<button class="h-12"> Svelte French Toast </button>
+	<button class="h-12"> TailwindCSS </button>
+
+	<button on:click={() => toast.success('This is a toast example!')}> Show Toast </button>
+</main>
