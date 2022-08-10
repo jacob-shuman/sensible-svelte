71b1329372e30585ae706497730d96d2d43d7cd8
Jacob Shuman <44483276+jacob-shuman@users.noreply.github.com>
2022-08-10 14:50:26 -0400

    chore: remove windicss

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,4 @@
 import { sveltekit } from '@sveltejs/kit/vite';
-import WindiCSS from 'vite-plugin-windicss';
 import typescript from '@rollup/plugin-typescript';
 
 /** @type {import('vite').UserConfig} */
